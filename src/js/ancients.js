dbde5ddd54bfebdc7691bff871774e6281924009
Lesnevsky-Sergey <sergeybaian@mail.ru>
2022-08-26 14:46:25 +0300

    feat:add project

---
+++ src/js/ancients.js
@@ -0,0 +1,106 @@
+// import Ancients from '../assets/Ancients/index'
+
+const ancientsData = [
+  {
+    id: 'azathoth',
+    name: 'azathoth',
+    allNumbers:{
+      green:5,
+      brown:9,
+      blue:2
+         },
+    // cardFace: Ancients.azathoth,
+    firstStage: {
+      greenCards: 1,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 2,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'cthulhu',
+    name: 'cthulhu',
+  //  cardFace: Ancients.cthulhu,
+   allNumbers:{
+green:4,
+brown:9,
+blue:2
+   },
+    firstStage: {
+      greenCards: 0,
+      blueCards: 2,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 1,
+      blueCards: 0,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'iogSothoth',
+    name: 'iogSothoth',
+    // cardFace: Ancients.iogSothoth,
+    allNumbers:{
+      green:5,
+      brown:9,
+      blue:2
+         },
+    firstStage: {
+      greenCards: 0,
+      blueCards: 1,
+      brownCards: 2,
+    },
+    secondStage: {
+      greenCards: 2,
+      blueCards: 1,
+      brownCards: 3,
+    },
+    thirdStage: {
+      greenCards: 3,
+      blueCards: 0,
+      brownCards: 4,
+    },
+  },
+  {
+    id: 'shubNiggurath',
+    name: 'shubNiggurath',
+    // cardFace: Ancients.shubNiggurath,
+    allNumbers:{
+      green:6,
+      brown:8,
+      blue:2
+         },
+    firstStage: {
+      greenCards: 1,
+      brownCards: 2,
+      blueCards: 1,
+    },
+    secondStage: {
+      greenCards: 3,
+      brownCards: 2,
+      blueCards: 1,
+    },
+    thirdStage: {
+      greenCards: 2,
+      brownCards: 4,
+      blueCards: 0,
+    },
+  },
+]
+
+export default ancientsData
