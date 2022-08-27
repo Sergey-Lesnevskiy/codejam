63dc9465e7cee4d534b625b68f3b1dccb9a06e19
Lesnevsky-Sergey <sergeybaian@mail.ru>
2022-08-27 15:36:58 +0300

    feat:block button when create the deck

--- src/js.js
+++ src/js.js
@@ -7,6 +7,9 @@ let changeCard = document.querySelector(".changeCard");
 // let card = document.querySelector(".card");
 let cardImg = document.querySelector(".card__img");
 let shuffle = document.querySelector(".shuffle__button");
+let ancients = document.querySelector(".ancients");
+
+
 
 let objBLUE = [];
 let objBROWN = [];
@@ -18,6 +21,10 @@ let stagesArrayCards = [[], [], []];
 let firstStageCards = [];
 let secondStageCards = [];
 let thirdStageCards = [];
+
+
+let targetClickId = ''
+let dataArray = []
 
 function severalRandom(min, max, num) {
   let i,
@@ -32,38 +39,49 @@ function severalRandom(min, max, num) {
   return res;
 }
 
-const shaffleCard = () => {
-  objBLUE = severalRandom(0, cardsDataBLUE.length - 1, 2).map((i) => {
+
+function dataFromAncients (id){
+  ancientsData.forEach((i, index)=>{
+    if( i.id ===id){
+     dataArray = [index, i.allNumbers]
+    }
+
+   })
+}
+
+const shaffleCard = (data) => {
+
+  objBLUE = severalRandom(0, cardsDataBLUE.length - 1, data[1].blue).map((i) => {
     return cardsDataBLUE[i];
   });
 
-  objBROWN = severalRandom(0, cardsDataBROWN.length - 1, 9).map((i) => {
+  objBROWN = severalRandom(0, cardsDataBROWN.length - 1, data[1].brown).map((i) => {
     return cardsDataBROWN[i];
   });
 
-  objGREEN = severalRandom(0, cardsDataGREEN.length - 1, 5).map((i) => {
+  objGREEN = severalRandom(0, cardsDataGREEN.length - 1, data[1].green).map((i) => {
     return cardsDataGREEN[i];
   });
 
-  cardsStage(ancientsData, "firstStage", firstStageCards);
-  cardsStage(ancientsData, "secondStage", secondStageCards);
-  cardsStage(ancientsData, "thirdStage", thirdStageCards);
+  cardsStage(ancientsData[data[0]], "firstStage", firstStageCards);
+  cardsStage(ancientsData[data[0]], "secondStage", secondStageCards);
+  cardsStage(ancientsData[data[0]], "thirdStage", thirdStageCards);
 
   stagesArrayCards = [firstStageCards, secondStageCards, thirdStageCards];
 };
 
 const cardsStage = (ancientsData, Stage, stagecards) => {
-  for (let i = 0; i < ancientsData[0][Stage].greenCards; i++) {
+  for (let i = 0; i < ancientsData[Stage].greenCards; i++) {
     stage.push(objGREEN[0]);
     stagecards.push(objGREEN[0]);
     objGREEN.shift(0);
   }
-  for (let i = 0; i < ancientsData[0][Stage].brownCards; i++) {
+  for (let i = 0; i < ancientsData[Stage].brownCards; i++) {
     stage.push(objBROWN[0]);
     stagecards.push(objBROWN[0]);
     objBROWN.shift(0);
   }
-  for (let i = 0; i < ancientsData[0][Stage].blueCards; i++) {
+  for (let i = 0; i < ancientsData[Stage].blueCards; i++) {
     stage.push(objBLUE[0]);
     stagecards.push(objBLUE[0]);
     objBLUE.shift(0);
@@ -75,7 +93,7 @@ let numberStageOut = 0;
 let numberArrStage = [];
 
 function changeCards(stageOder, numberPicture, numberStage) {
-  //  console.log(numberArrStage.length)
+
 
   if (numberArrStage.length === 0) {
     numberArrStage = severalRandom(
@@ -84,12 +102,7 @@ function changeCards(stageOder, numberPicture, numberStage) {
       stageOder[numberStage].length
     );
   }
-  console.log(
-    "#PICT=",
-    numberPicture,
-    "  #stageOder[numberStage].length=",
-    stageOder[numberStage].length
-  );
+ 
   if (numberPicture > stageOder[numberStage].length - 1) {
     cardImg.src = "assets/mythicCardBackground.png";
     document.querySelector(
@@ -101,14 +114,17 @@ function changeCards(stageOder, numberPicture, numberStage) {
 
     if (numberStageOut >= stageOder.length) {
       cardImg.src = "assets/mythicCardBackground.png";
-     
+      ancients.addEventListener('click',changeAncients)
+      shuffle.addEventListener("click", shuffleButtonBlock);
       changeCard.style.display = "none";
+      shuffle.style.display = "none";
       numberStageOut = 0;
       stagesArrayCards = [];
       firstStageCards = [];
       secondStageCards = [];
       thirdStageCards = [];
-      numberArrStage=[]
+      numberArrStage=[];
+      stage = [];
     }
   } else {
     cardImg.src =
@@ -124,30 +140,53 @@ function removeValues() {
   }
 }
 
-shuffle.addEventListener("click", () => {
-  // document.querySelector('.nameStage').remove()
-removeValues();
-
-  shaffleCard();
-  changeCard.style.display = "block";
-
+function changeAncients(event){
+  shuffle.style.display = "block";
   
+  if(changeCard.style.display === "block") {
+      this.removeEventListener('click',changeAncients);
+  }else{
+    if(targetClickId){
+    document.getElementById(`${targetClickId}`).classList.remove('active')
+       targetClickId=event.target.id
+      event.target.className += " active"
+    }else{
+event.target.className += " active"
+targetClickId = event.target.id 
+    }
+  }
+    }
 
-  // console.log(numberArrStage);
-  // console.log(numberStageOut);
-  // console.log(numberPictureOut);
-  // console.log(stagesArrayCards);
-  // console.log(firstStageCards);
-  // console.log(secondStageCards);
-  // console.log(thirdStageCards);
+    function setSessionStorage(){
+      sessionStorage.setItem(`${targetClickId}`,JSON.stringify(stage))
+      console.log(sessionStorage.getItem(`${targetClickId}`));
+    }
 
-});
+function shuffleButtonBlock(){
+
+  if(changeCard.style.display === "block"){
+    this.removeEventListener("click", shuffleButtonBlock);
+  }else{
+      dataFromAncients(targetClickId)
+   removeValues();
+   shaffleCard(dataArray);
+  changeCard.style.display = "block";
+  setSessionStorage()
+  }
+}
+
+
+ancients.addEventListener('click',changeAncients)
+
+shuffle.addEventListener("click", shuffleButtonBlock);
+
  changeCard.addEventListener("click", () => {
     
     changeCards(stagesArrayCards, numberPictureOut, numberStageOut);
   });
-// ancientsData.forEach((i,index)=>{
-//   if(i.id=='shubNiggurath'){
-//     return console.log(index)
-//   }
-// })
+
+ 
+ 
+
+
+
